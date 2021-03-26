1e06682e64195412c8fa8951682a350b6c7ed8e9
Swapnil Narwade <81177774+Now-Tiger@users.noreply.github.com>
2021-03-27 02:33:02 +0530

    change font size simple console
    change font size simple console

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Change_font_size
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(219, 102);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(42, 13);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "HELLO";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(172, 130);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(143, 55);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Change Font ";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(199, 191);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(91, 34);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "Exit !";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 294);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+    }
+}
+
