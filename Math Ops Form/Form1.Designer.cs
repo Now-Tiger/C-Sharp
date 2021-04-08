94b56277ab0cf558aa83061cce489f61b1a0ac00
Swapnil Narwade <81177774+Now-Tiger@users.noreply.github.com>
2021-04-09 03:01:03 +0530

    Math window with radio buttons options.

---
+++ Math Ops Form/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace MATH
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCal = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtAns = new System.Windows.Forms.TextBox();
+            this.lblNumber1 = new System.Windows.Forms.Label();
+            this.lblNumer2 = new System.Windows.Forms.Label();
+            this.lblAns = new System.Windows.Forms.Label();
+            this.radBtn1 = new System.Windows.Forms.RadioButton();
+            this.radBtn2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radBtn4 = new System.Windows.Forms.RadioButton();
+            this.lblOperator = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCal
+            // 
+            this.btnCal.Location = new System.Drawing.Point(415, 135);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(75, 23);
+            this.btnCal.TabIndex = 0;
+            this.btnCal.Text = "Calculate";
+            this.btnCal.UseVisualStyleBackColor = true;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(415, 179);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(42, 91);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 2;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(42, 151);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.TabIndex = 3;
+            // 
+            // txtAns
+            // 
+            this.txtAns.Location = new System.Drawing.Point(402, 94);
+            this.txtAns.Name = "txtAns";
+            this.txtAns.Size = new System.Drawing.Size(100, 20);
+            this.txtAns.TabIndex = 4;
+            // 
+            // lblNumber1
+            // 
+            this.lblNumber1.AutoSize = true;
+            this.lblNumber1.Location = new System.Drawing.Point(39, 75);
+            this.lblNumber1.Name = "lblNumber1";
+            this.lblNumber1.Size = new System.Drawing.Size(53, 13);
+            this.lblNumber1.TabIndex = 5;
+            this.lblNumber1.Text = "Number 1";
+            // 
+            // lblNumer2
+            // 
+            this.lblNumer2.AutoSize = true;
+            this.lblNumer2.Location = new System.Drawing.Point(39, 135);
+            this.lblNumer2.Name = "lblNumer2";
+            this.lblNumer2.Size = new System.Drawing.Size(53, 13);
+            this.lblNumer2.TabIndex = 6;
+            this.lblNumer2.Text = "Number 2";
+            // 
+            // lblAns
+            // 
+            this.lblAns.AutoSize = true;
+            this.lblAns.Location = new System.Drawing.Point(399, 75);
+            this.lblAns.Name = "lblAns";
+            this.lblAns.Size = new System.Drawing.Size(37, 13);
+            this.lblAns.TabIndex = 7;
+            this.lblAns.Text = "Result";
+            // 
+            // radBtn1
+            // 
+            this.radBtn1.AutoSize = true;
+            this.radBtn1.Location = new System.Drawing.Point(224, 73);
+            this.radBtn1.Name = "radBtn1";
+            this.radBtn1.Size = new System.Drawing.Size(78, 17);
+            this.radBtn1.TabIndex = 8;
+            this.radBtn1.TabStop = true;
+            this.radBtn1.Text = "Addition (+)";
+            this.radBtn1.UseVisualStyleBackColor = true;
+            this.radBtn1.CheckedChanged += new System.EventHandler(this.radBtn1_CheckedChanged);
+            // 
+            // radBtn2
+            // 
+            this.radBtn2.AutoSize = true;
+            this.radBtn2.Location = new System.Drawing.Point(224, 110);
+            this.radBtn2.Name = "radBtn2";
+            this.radBtn2.Size = new System.Drawing.Size(96, 17);
+            this.radBtn2.TabIndex = 9;
+            this.radBtn2.TabStop = true;
+            this.radBtn2.Text = "Substraction (-)";
+            this.radBtn2.UseVisualStyleBackColor = true;
+            this.radBtn2.CheckedChanged += new System.EventHandler(this.radBtn2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(224, 145);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(100, 17);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Multiplication (x)";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radBtn4
+            // 
+            this.radBtn4.AutoSize = true;
+            this.radBtn4.Location = new System.Drawing.Point(224, 183);
+            this.radBtn4.Name = "radBtn4";
+            this.radBtn4.Size = new System.Drawing.Size(76, 17);
+            this.radBtn4.TabIndex = 11;
+            this.radBtn4.TabStop = true;
+            this.radBtn4.Text = "Division (/)";
+            this.radBtn4.UseVisualStyleBackColor = true;
+            this.radBtn4.CheckedChanged += new System.EventHandler(this.radBtn4_CheckedChanged);
+            // 
+            // lblOperator
+            // 
+            this.lblOperator.AutoSize = true;
+            this.lblOperator.Location = new System.Drawing.Point(211, 39);
+            this.lblOperator.Name = "lblOperator";
+            this.lblOperator.Size = new System.Drawing.Size(53, 13);
+            this.lblOperator.TabIndex = 12;
+            this.lblOperator.Text = "Operators";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 282);
+            this.Controls.Add(this.lblOperator);
+            this.Controls.Add(this.radBtn4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radBtn2);
+            this.Controls.Add(this.radBtn1);
+            this.Controls.Add(this.lblAns);
+            this.Controls.Add(this.lblNumer2);
+            this.Controls.Add(this.lblNumber1);
+            this.Controls.Add(this.txtAns);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCal);
+            this.Name = "frmMain";
+            this.Text = "MATH";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCal;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtAns;
+        private System.Windows.Forms.Label lblNumber1;
+        private System.Windows.Forms.Label lblNumer2;
+        private System.Windows.Forms.Label lblAns;
+        private System.Windows.Forms.RadioButton radBtn1;
+        private System.Windows.Forms.RadioButton radBtn2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radBtn4;
+        private System.Windows.Forms.Label lblOperator;
+    }
+}
+
