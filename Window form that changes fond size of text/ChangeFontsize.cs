58e45558074e9f7f1e4452385a808ff1c1fac77b
Swapnil Narwade <81177774+Now-Tiger@users.noreply.github.com>
2021-04-25 03:41:17 +0530

    Add files via upload

---
+++ Window form that changes fond size of text/ChangeFontsize.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Change_font_size
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            FontDialog dig = new FontDialog();
+            dig.ShowDialog();
+            lbl1.Font = dig.Font;
+        }
+    }
+}
